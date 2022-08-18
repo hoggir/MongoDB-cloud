e0689e7dd74d6d8ce6c02527ddcd57bea7412989
Fadel Giras <fadelpopeye@gmail.com>
2022-08-18 23:32:19 +0700

    add.env

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
+require("./config/mongoose");
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const productRouter = require("./app/product/router");
 var cors = require("cors");
 const PORT = process.env.PORT || 3000;
-const { secretkey } = require("./config/config");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-
-const url = secretkey;
-
-const connectionParams = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-mongoose
-  .connect(url, connectionParams)
-  .then(() => {
-    console.log("Connected to the database ");
-  })
-  .catch((err) => {
-    console.error(`Error connecting to the database. n${err}`);
-  });
 
 app.get("/", (req, res) => {
   res.send({ status: "oke" });
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://admin:admin@cluster0.qqahlie.mongodb.net/?retryWrites=true&w=majority"
-);
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", () => console.log("server mongoose terhubung"));
+const { secretkey } = require("./config");
+
+const url = secretkey;
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const db = mongoose
+  .connect(url, connectionParams)
+  .then(() => {
+    console.log("Connected to the database ");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. n${err}`);
+  });
+
+module.exports = db;
